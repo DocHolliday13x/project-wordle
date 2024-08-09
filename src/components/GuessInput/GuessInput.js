38f281703255cc12336481c40d9720f68adbfa9d
ryan eastman <rking1323@gmail.com>
2024-08-09 15:18:24 -0500

    completed GuessInput component w/added input validation

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,10 +1,52 @@
 import React from 'react';
 
 function GuessInput() {
-  return <form className="guess-input-wrapper">
-  <label htmlFor="guess-input">Enter guess:</label>
-  <input id="guess-input" type="text" />
-</form>;
+  // Create a state hook to store the current guess.
+  // The initial value of the guess is an empty string.
+  const [guess, setGuess] = React.useState('');
+
+  function handleSubmit(event) {
+    // Prevent the form from refreshing the page.
+    event.preventDefault();
+    // Adding some validation for the guess.
+    if (!guess) {
+      // If the guess is empty, return early and do nothing.
+      return;
+    }
+
+    if (guess.length !== 5) {
+      window.alert('Guess must be 5 characters long.');
+      return;
+    }
+
+    // Log the current guess as an object to the console.
+    console.log({ guess });
+    // Clear the input after submitting the form.
+    setGuess('');
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="guess-input-wrapper">
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        required
+        minLength={5}
+        maxLength={5}
+        pattern="[a-zA-Z]{5}"
+        title="5 Letter Word"
+        // The value of the input is the current guess.
+        value={guess}
+        // Add onChange event listener to update the guess.
+        onChange={(event) => {
+          // Update the guess with the value of the input.
+          const nextGuess = event.target.value.toUpperCase();
+          setGuess(nextGuess);
+        }}
+        id="guess-input" 
+        type="text" 
+      />
+    </form>
+  );
 }
 
 export default GuessInput;
