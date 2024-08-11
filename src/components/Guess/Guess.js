102dd40d1230a16d4402db1fd51a7668b6e43fee
ryan eastman <rking1323@gmail.com>
2024-08-11 09:05:48 -0500

    added Guess component to GuessResults

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,7 +1,45 @@
 import React from 'react';
 
-function Guess() {
-  return <div></div>;
+function Guess({ guess }) {
+  return (
+    <div className="guess-results">
+  <p className="guess">
+    <span className="cell">H</span>
+    <span className="cell">E</span>
+    <span className="cell">L</span>
+    <span className="cell">L</span>
+    <span className="cell">O</span>
+  </p>
+  <p className="guess">
+    <span className="cell">T</span>
+    <span className="cell">H</span>
+    <span className="cell">E</span>
+    <span className="cell">R</span>
+    <span className="cell">E</span>
+  </p>
+  <p className="guess">
+    <span className="cell">W</span>
+    <span className="cell">O</span>
+    <span className="cell">R</span>
+    <span className="cell">L</span>
+    <span className="cell">D</span>
+  </p>
+  <p className="guess">
+    <span className="cell"></span>
+    <span className="cell"></span>
+    <span className="cell"></span>
+    <span className="cell"></span>
+    <span className="cell"></span>
+  </p>
+  <p className="guess">
+    <span className="cell"></span>
+    <span className="cell"></span>
+    <span className="cell"></span>
+    <span className="cell"></span>
+    <span className="cell"></span>
+  </p>
+</div>
+  )
 }
 
 export default Guess;
