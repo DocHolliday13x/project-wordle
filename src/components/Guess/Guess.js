8a2156b8a7e66c1d6b0529db7e90095733818dbf
ryan eastman <rking1323@gmail.com>
2024-08-11 10:26:17 -0500

    Completed exercise 3 and 4.

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -2,7 +2,21 @@ import React from "react";
 import { range } from "../../utils";
 import { checkGuess } from "../../game-helpers"; // Josh provided this function in the game-helpers.js file to help us out.
 
-function Guess({ value, answer }) { // Added new prop "answer"
+// Creating a function for the cells to have a dynamic class name.
+function Cell({ letter, status }) {
+  // Added the letter and status props so we can use them to determine the class name.
+  const className = status ? `cell ${status}` : "cell"; // If there's a status, use it. Otherwise, use "cell".
+
+  return (
+    <span className={className}>
+      {letter}{" "}
+      {/* I'm going to have to change the className from "cell" to something dynamic. Achieved by adding className variable above */}
+    </span>
+  );
+}
+
+function Guess({ value, answer }) {
+  // Added new prop "answer"
   // We'll use the checkGuess function to determine if the guess is correct.
   const result = checkGuess(value, answer);
 
@@ -11,9 +25,11 @@ function Guess({ value, answer }) { // Added new prop "answer"
   return (
     <p className="guess">
       {range(5).map((num) => (
-        <span key={num} className="cell">
-          {value ? value[num] : ""}
-        </span>
+        <Cell
+          key={num}
+          letter={result ? result[num].letter : ""} // Pass the letter prop to the Cell component. Adding the check for result to prevent errors.
+          status={result ? result[num].status : ""} // Pass the status prop to the Cell component.
+        /> // Pass the value and num props to the Cell component.
       ))}
     </p>
   );
